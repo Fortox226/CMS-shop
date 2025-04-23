88d846c84f4d12604fe29d88a5be31a5406177cd
Fortox226 <97980099+Fortox226@users.noreply.github.com>
2025-04-23 13:11:14 +0200

    układ strony + footer

---
+++ php/article.php
@@ -0,0 +1,55 @@
+<?php 
+include 'config.php';
+
+if (isset($_GET['id']) && !empty($_GET['id'])) {
+    $id_article = $_GET['id'];
+    
+    
+    $sql = "SELECT * FROM article WHERE id_article = '$id_article'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $title = $row['title'];
+        $content = $row['content'];
+        $date = $row['date'];
+        $image = $row['header_image'];
+    } else {
+        echo "Artykuł nie został znaleziony.";
+        $title = $content = $date = ''; 
+    }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="../css/style-article.css">
+</head>
+<body>
+    <header>
+        <div class="header-bar">
+            <div class="title">
+                <h1>CMS Articles</h1>
+            </div>
+            <div class="back">
+                <a href="index.php">Strona główna</a>
+            </div>
+        </div>
+    </header>
+    <main>
+        <?php
+            echo "<div class='article'>";
+            echo "<img src=\"" .  htmlspecialchars($image) . "\" alt=\"" . htmlspecialchars($row['title']) . "\"/>";
+            echo "<h1>" . htmlspecialchars($title) . "</h1>";
+            echo "<div class='content'>" . nl2br(htmlspecialchars($content)) . "</div>";
+            echo "</div>";
+        ?>
+    </main>
+</body>
+</html>
